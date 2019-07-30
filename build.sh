54c8f291939511a2e273e55c9ba754f5e048a0cf
Vladislav Klimenok <vladyslav.klymenok@jevera.software>
2019-07-30 11:30:24 +0300

    Deleted zip into archive

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
+#!/usr/bin/env bash
 docker rm -f $(docker ps -a -q ) || docker image rm $(docker images -a -q)
 docker build -t web-app-side-menu .
 docker run --name main -d -p 3000:80 web-app-side-menu
-zip -r ../todo-app.zip .
 
